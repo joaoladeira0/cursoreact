5c7d8560b96f3d243469832ac7ec494ef8316460
joaoladeira0 <124743276+joaoladeira0@users.noreply.github.com>
2024-12-27 11:41:23 -0300

    2

---
+++ 009 - CONTEXT API/context/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Ronaldo</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
