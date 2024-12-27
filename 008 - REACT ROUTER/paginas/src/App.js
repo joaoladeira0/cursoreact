1d4d5316c1caa7884434477a894a9a00672bcb2c
joaoladeira0 <124743276+joaoladeira0@users.noreply.github.com>
2024-12-27 10:38:24 -0300

    1

--- 008 - REACT ROUTER/paginas/src/App.js
+++ 008 - REACT ROUTER/paginas/src/App.js
@@ -13,6 +13,7 @@ import Product from './pages/Product';
 import Info from './pages/Info';
 import NotFound from './pages/NotFound';
 import SearchForm from './pages/SearchForm';
+import Search from './pages/Search';
 
 function App() {
   return (
@@ -30,6 +31,8 @@ function App() {
           <Route path='/products/:id/info' element={<Info />} />
           {/* 4 - rota dinamica */}
           <Route path='/products/:id' element={<Product />} />
+          {/* 9 - search */}
+          <Route path='/search' element={<Search />} />
           {/* 7 - no match route */}
           <Route path="/*" element={<NotFound />} />
         </Routes>
